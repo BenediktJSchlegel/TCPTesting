a1737ff51a8d5a9c42fa060b053ea30d9c1f190a
BenediktJSchlegel <benediktjschlegel@gmail.com>
2021-01-14 19:22:07 +0100

    Add project files.

---
+++ SocketSending/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MainClass
+{
+    public static void Main()
+    {
+        IPEndPoint ip = new IPEndPoint(IPAddress.Parse("x.x.x.x"), 6555);
+
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            server.Connect(ip);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("Unable to connect to server: " + e);
+            return;
+        }
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            server.Send(Encoding.ASCII.GetBytes(input));
+
+            Console.WriteLine("Sent:");
+            Console.WriteLine(Environment.NewLine);
+        }
+
+    }
+}
+
---
+++ TCPReceiverApp/TCPReceiverApp/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TCPReceiverApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private string statusMessage = "Waiting for Socket-Connection . . .";
+        public string StatusMessage
+        {
+            get => statusMessage;
+            set 
+            {
+                statusMessage = value;
+                OnPropertyChanged(nameof(StatusMessage));
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            Thread socketThread = new Thread(SocketStuff);
+            socketThread.Start();
+        }
+
+        private void SocketStuff()
+        {
+            try
+            {
+                TcpListener tcp = new TcpListener(IPAddress.Parse("x.x.x.x"), 6555);
+                tcp.Start();
+
+                Socket socket = null;
+
+                while (true)
+                {
+
+                    if (socket != null)
+                    {
+
+                        byte[] data = new byte[1024];
+                        int receivedDataLength = socket.Receive(data);
+                        string stringData = Encoding.ASCII.GetString(data, 0, receivedDataLength);
+                        StatusMessage = stringData;
+                    }
+                    else
+                    {
+                        socket = tcp.AcceptSocket();
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
